5b1fb6bfff35592b6e93c991cb92d2ed6477f96c
Thomas Crider <gloriouseggroll@gmail.com>
2022-08-05 11:15:32 -0600

    rebase for 7.14

--- patches/protonprep-LoL.sh
+++ patches/protonprep-LoL.sh
@@ -10,7 +10,7 @@
     cd wine
     git reset --hard HEAD
     git clean -xdf
-
+    
     echo "applying staging patches"
     ../wine-staging/patches/patchinstall.sh DESTDIR="." --all
 
@@ -28,26 +28,17 @@
 
     echo "proton QPC performance patch"
     patch -Np1 < ../patches/proton/49-proton_QPC-update-replace.patch
-
+        
     echo "proton LFH performance patch"
-    patch -Np1 < ../patches/wine-hotfixes/LoL/lfh-non-proton-pre-needed.patch
     patch -Np1 < ../patches/proton/50-proton_LFH.patch
-    
-    echo "update vulkan to 1.3 for dxvk-master support"
-    patch -Np1 < ../patches/winevulkan/1.2.203.patch
-    patch -Np1 < ../patches/winevulkan/1.3.204.patch
-    patch -Np1 < ../patches/winevulkan/1.3-support.patch
-    patch -Np1 < ../patches/winevulkan/1.3-fix-prototype-mismatch.patch
-    patch -Np1 < ../patches/winevulkan/1.3-add-support-win32-long-types.patch
 
     echo "LoL fixes"
     patch -Np1 < ../patches/wine-hotfixes/LoL/LoL-6.17+-syscall-fix.patch
     patch -Np1 < ../patches/wine-hotfixes/LoL/LoL-abi.vsyscall32-alternative_patch_by_using_a_fake_cs_segment.patch
     patch -Np1 < ../patches/wine-hotfixes/LoL/LoL-broken-client-update-fix.patch
-    patch -Np1 < ../patches/wine-hotfixes/LoL/LoL-launcher-client-connectivity-fix-0001-ws2_32-Return-a-valid-value-for-WSAIoctl-SIO_IDEAL_S.patch
-    patch -Np1 < ../patches/wine-hotfixes/LoL/LoL-garena-childwindow.patch
     patch -Np1 < ../patches/wine-hotfixes/LoL/LoL-client-slow-start-fix.patch
     patch -Np1 < ../patches/wine-hotfixes/LoL/LoL-abi-vsyscall-fix.patch
+    patch -Np1 < ../patches/wine-hotfixes/LoL/LoL-garena-childwindow.patch
 
     echo "cleanup .orig files"
     find ./ -name '*.orig' -delete
