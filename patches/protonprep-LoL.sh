0496875d7d53694854375d8f536e215ba0df0e22
kyechou <kuanyenchou@gmail.com>
2023-04-11 09:28:20 -0500

    Port changes from #10 to GE-LoL 7.0

--- patches/protonprep-LoL.sh
+++ patches/protonprep-LoL.sh
@@ -17,11 +17,11 @@
     echo "clock monotonic"
     patch -Np1 < ../patches/proton/01-proton-use_clock_monotonic.patch
 
-    echo "applying fsync patches"
-    patch -Np1 < ../patches/proton/03-proton-fsync_staging.patch
-
-    echo "proton futex waitv patches"
-    patch -Np1 < ../patches/proton/57-fsync_futex_waitv.patch
+    # Client won't launch with fsync patches
+    # echo "applying fsync patches"
+    # patch -Np1 < ../patches/proton/03-proton-fsync_staging.patch
+    # echo "proton futex waitv patches"
+    # patch -Np1 < ../patches/proton/57-fsync_futex_waitv.patch
 
     echo "LAA"
     patch -Np1 < ../patches/proton/04-proton-LAA_staging.patch
@@ -32,7 +32,7 @@
     echo "proton LFH performance patch"
     patch -Np1 < ../patches/wine-hotfixes/LoL/lfh-non-proton-pre-needed.patch
     patch -Np1 < ../patches/proton/50-proton_LFH.patch
-    
+
     echo "update vulkan to 1.3 for dxvk-master support"
     patch -Np1 < ../patches/winevulkan/1.2.203.patch
     patch -Np1 < ../patches/winevulkan/1.3.204.patch
@@ -41,14 +41,12 @@
     patch -Np1 < ../patches/winevulkan/1.3-add-support-win32-long-types.patch
 
     echo "LoL fixes"
-    patch -Np1 < ../patches/wine-hotfixes/LoL/LoL-6.17+-syscall-fix.patch
-    patch -Np1 < ../patches/wine-hotfixes/LoL/LoL-abi.vsyscall32-alternative_patch_by_using_a_fake_cs_segment.patch
     patch -Np1 < ../patches/wine-hotfixes/LoL/LoL-broken-client-update-fix.patch
     patch -Np1 < ../patches/wine-hotfixes/LoL/LoL-launcher-client-connectivity-fix-0001-ws2_32-Return-a-valid-value-for-WSAIoctl-SIO_IDEAL_S.patch
-    patch -Np1 < ../patches/wine-hotfixes/LoL/LoL-garena-childwindow.patch
     patch -Np1 < ../patches/wine-hotfixes/LoL/LoL-client-slow-start-fix.patch
-    patch -Np1 < ../patches/wine-hotfixes/LoL/LoL-abi-vsyscall-fix.patch
-    
+    patch -Np1 < ../patches/wine-hotfixes/LoL/LoL-seh-assertion-failure.patch
+    patch -Np1 < ../patches/wine-hotfixes/LoL/LoL-ntdll-nopguard-call_vectored_handlers.patch
+
     echo "Custom"
     patch -Np1 < ../patches/custom/hide_prefix_update_window.patch
 
