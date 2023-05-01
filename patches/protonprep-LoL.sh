f4c4df5bde6dd024067abdc3e524da572e8507cc
GloriousEggroll <gloriouseggroll@gmail.com>
2023-04-30 22:11:31 -0600

    7.5+ WIP

--- patches/protonprep-LoL.sh
+++ patches/protonprep-LoL.sh
@@ -14,41 +14,45 @@
     echo "applying staging patches"
     ../wine-staging/patches/patchinstall.sh DESTDIR="." --all
 
-    echo "clock monotonic"
-    patch -Np1 < ../patches/proton/01-proton-use_clock_monotonic.patch
+#    echo "clock monotonic"
+#    patch -Np1 < ../patches/proton/01-proton-use_clock_monotonic.patch
 
-    echo "applying fsync patches"
-    patch -Np1 < ../patches/proton/03-proton-fsync_staging.patch
+#    echo "applying fsync patches"
+#    patch -Np1 < ../patches/proton/03-proton-fsync_staging.patch
 
-    echo "proton futex waitv patches"
-    patch -Np1 < ../patches/proton/57-fsync_futex_waitv.patch
+#    echo "proton futex waitv patches"
+#    patch -Np1 < ../patches/proton/57-fsync_futex_waitv.patch
 
-    echo "LAA"
-    patch -Np1 < ../patches/proton/04-proton-LAA_staging.patch
+#    echo "LAA"
+#    patch -Np1 < ../patches/proton/04-proton-LAA_staging.patch
 
-    echo "proton QPC performance patch"
-    patch -Np1 < ../patches/proton/49-proton_QPC-update-replace.patch
+#    echo "proton QPC performance patch"
+#    patch -Np1 < ../patches/proton/49-proton_QPC-update-replace.patch
 
-    echo "proton LFH performance patch"
-    patch -Np1 < ../patches/wine-hotfixes/LoL/lfh-non-proton-pre-needed.patch
-    patch -Np1 < ../patches/proton/50-proton_LFH.patch
+#    echo "proton LFH performance patch"
+#    patch -Np1 < ../patches/wine-hotfixes/LoL/lfh-non-proton-pre-needed.patch
+#    patch -Np1 < ../patches/proton/50-proton_LFH.patch
 
-    echo "update vulkan to 1.3 for dxvk-master support"
-    patch -Np1 < ../patches/winevulkan/1.2.203.patch
-    patch -Np1 < ../patches/winevulkan/1.3.204.patch
-    patch -Np1 < ../patches/winevulkan/1.3-support.patch
-    patch -Np1 < ../patches/winevulkan/1.3-fix-prototype-mismatch.patch
-    patch -Np1 < ../patches/winevulkan/1.3-add-support-win32-long-types.patch
+#    echo "update vulkan to 1.3 for dxvk-master support"
+#    patch -Np1 < ../patches/winevulkan/1.2.203.patch
+#    patch -Np1 < ../patches/winevulkan/1.3.204.patch
+#    patch -Np1 < ../patches/winevulkan/1.3-support.patch
+#    patch -Np1 < ../patches/winevulkan/1.3-fix-prototype-mismatch.patch
+#    patch -Np1 < ../patches/winevulkan/1.3-add-support-win32-long-types.patch
 
     echo "LoL fixes"
+    patch -Np1 < ../patches/wine-hotfixes/LoL/LoL-revert-commits-that-cause-LoL-client-post-match-hang-7.6+.patch
     patch -Np1 < ../patches/wine-hotfixes/LoL/LoL-broken-client-update-fix.patch
-    patch -Np1 < ../patches/wine-hotfixes/LoL/LoL-launcher-client-connectivity-fix-0001-ws2_32-Return-a-valid-value-for-WSAIoctl-SIO_IDEAL_S.patch
+    #patch -Np1 < ../patches/wine-hotfixes/LoL/LoL-launcher-client-connectivity-fix-0001-ws2_32-Return-a-valid-value-for-WSAIoctl-SIO_IDEAL_S.patch
     patch -Np1 < ../patches/wine-hotfixes/LoL/LoL-client-slow-start-fix.patch
-    patch -Np1 < ../patches/wine-hotfixes/LoL/LoL-seh-assertion-failure.patch
+    #patch -Np1 < ../patches/wine-hotfixes/LoL/LoL-seh-assertion-failure.patch
     patch -Np1 < ../patches/wine-hotfixes/LoL/LoL-ntdll-nopguard-call_vectored_handlers.patch
 
-    echo "Custom"
-    patch -Np1 < ../patches/custom/hide_prefix_update_window.patch
+#    # hotfix 7.5 winealsa compile breakage
+#    patch -Np1 < ../patches/wine-hotfixes/pending/7.5-winealsa-hotfix.patch
+
+#    echo "Custom"
+#    patch -Np1 < ../patches/custom/hide_prefix_update_window.patch
 
     echo "cleanup .orig files"
     find ./ -name '*.orig' -delete
