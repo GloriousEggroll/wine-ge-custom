891f4cda2805451d685addc5985ec79a577b862b
GloriousEggroll <gloriouseggroll@gmail.com>
2023-09-20 20:58:24 -0600

    update buildbot

--- makebuild.sh
+++ makebuild.sh
@@ -8,18 +8,15 @@ if [[ ! -d vagrant_share ]]; then
 	mkdir -p vagrant_share
 fi
 if [[ -z $(podman container list -a | grep buildbot) ]]; then
-	docker create --interactive --name buildbot --mount type=bind,source="$PWD"/vagrant_share,destination=/vagrant,rw=true docker.io/gloriouseggroll/lutris_buildbot:bookworm
+	docker create --interactive --name buildbot --mount type=bind,source="$PWD"/vagrant_share,destination=/vagrant,readonly=false --mount type=bind,source="$PWD"/buildbot,destination=/home/vagrant/buildbot,readonly=false docker.io/gloriouseggroll/lutris_buildbot:latest
 fi
 
 docker start buildbot
 
 # cleanup any old builds first
-docker exec buildbot bash -c "cd /home/vagrant/lutris-buildbot && git config --global --add safe.directory /home/vagrant/lutris-buildbot"
-docker exec buildbot bash -c "cd /home/vagrant/lutris-buildbot && git reset --hard HEAD && git clean -xdf && git pull"
-docker exec buildbot bash -c "rm -Rf /home/vagrant/lutris-buildbot/runners/wine/wine-src/"
+docker exec buildbot bash -c "rm -Rf /home/vagrant/buildbot/runners/wine/wine-src/"
 
 # start build
-docker exec buildbot bash -c "cd /home/vagrant/lutris-buildbot/runners/wine && ./build.sh --as $1 --version $3 --with $2 --branch $3"
+docker exec buildbot bash -c "cd /home/vagrant/buildbot/runners/wine && ./build.sh --as $1 --version $3 --with $2 --branch $3"
 
 docker stop buildbot
-
