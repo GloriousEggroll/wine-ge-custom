659f9d129d520b4f66134c1ecc45d8af763fe5a1
GloriousEggroll <gloriouseggroll@gmail.com>
2023-09-23 23:52:34 -0600

    buildbot: update buildbot

--- makebuild.sh
+++ makebuild.sh
@@ -4,11 +4,11 @@
 # example: ./makebuild.sh lutris-GE https://github.com/GloriousEggroll/proton-wine Proton8-15
 # build name output: vagrant_share/wine-lutris-GE-Proton8-15-x86_64.tar.xz
 
-if [[ ! -d vagrant_share ]]; then
-	mkdir -p vagrant_share
+if [[ ! -d builds ]]; then
+	mkdir -p builds
 fi
 if [[ -z $(podman container list -a | grep buildbot) ]]; then
-	docker create --interactive --name buildbot --mount type=bind,source="$PWD"/vagrant_share,destination=/vagrant,readonly=false --mount type=bind,source="$PWD"/buildbot,destination=/home/vagrant/buildbot,readonly=false docker.io/gloriouseggroll/lutris_buildbot:latest
+	docker create --interactive --name buildbot --mount type=bind,source="$PWD"/builds,destination=/builds,readonly=false --mount type=bind,source="$PWD"/buildbot,destination=/home/vagrant/buildbot,readonly=false docker.io/gloriouseggroll/lutris_buildbot:latest
 fi
 
 docker start buildbot
